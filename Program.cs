6ccf0013de7dda4e6c9cc1e0192764d8f325b653
UltrascatterRED <natbarton14@gmail.com>
2023-04-21 22:35:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1260_001_BartonNathaniel_Project5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Room.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1260_001_BartonNathaniel_Project5
+{
+    internal class Room
+    {
+        private bool[] Exits = new bool[4];
+        private bool HasMonster;
+        //private Monster Monster;
+        private bool HasWeapon;
+        //private Weapon Weapon;
+        private int[] position = new int[2];
+
+        public string DescString()
+        {
+            StringBuilder bldr = new StringBuilder();
+            Random rand = new Random();
+            bldr.Append("You step into another room, just as dim and" +
+                "unwelcoming as the last.\n");
+            if (HasWeapon)
+            {
+                bldr.Append("There is a [weapon] propped against one wall."); //link to generation logic to determine weapon type
+            }
+            if(HasMonster)
+            {
+
+            }
+        }
+    }
+}
